b4c47ba3dc6c42e96695b84bbdab1ffb50e16c45
Eugene G <eugene7@gmail.com>
2014-10-06 19:32:59 +1100

    Optimizing SVG output

--- index.js
+++ index.js
@@ -3,89 +3,88 @@
 var request = require('superagent'),
     path    = require('path'),
     fs      = require('graceful-fs'),
+    svgo    = new (require('svgo')),
     mkdir   = require('mkdirp'),
     config  = require('./defaultConfig')
     ;
 
-function splitupSvgContent(fontSvgDefinition, fontNameRefs) {
+function splitupSvgContent(fontSvgDefinition, fontNameRefs, callback) {
     var iconNamingConventions = require('js-yaml').safeLoad(fontNameRefs).icons,
         fontAwesomeGlyphs = fontSvgDefinition.match(/<glyph.*?d=.{10,}?>/g), //filter for glyphs with real path specified
         defaultCharWidth = fontSvgDefinition.match(/<font id="fontawesomeregular" horiz-adv-x="(\d+)"/)[1],
         defaultCharHeight = fontSvgDefinition.match(/<font-face units-per-em="(\d+)"/)[1],
         iconNameByCode = {},
-        iconSvgCodes = {};
+        iconSvg = []
+        ;
 
-
-    console.info("Found " + fontAwesomeGlyphs.length + " available icons in the font");
-    console.info("Generating SVG data for each icon...");
 
     iconNamingConventions.forEach(function (icon) {
         iconNameByCode[icon.unicode] = icon.id;
     });
 
+    console.info("Found " + fontAwesomeGlyphs.length + " available icons in the font");
+    console.info("Generating SVG data for each icon...");
+
     fontAwesomeGlyphs.forEach(function (glyph) {
         var iconCode = glyph.match(/unicode="&#x([a-f0-9]+);"/)[1],
+            iconName = iconNameByCode[iconCode],
             pathData = glyph.match(/d="(.+?)"/)[1],
             customWidthMatch = glyph.match(/horiz-adv-x="(.*?)"/),
-            contentWidth = customWidthMatch ? customWidthMatch[1] : defaultCharWidth,
-            iconName = iconNameByCode[iconCode];
-
-        if (!iconName) console.log(glyph);
+            contentWidth = customWidthMatch ? customWidthMatch[1] : defaultCharWidth
+            ;
 
         if (config.icons.length && config.icons.indexOf(iconName) == -1) {
             return;
         }
-        config.height.forEach(function (svgHeight) {
-            var scalingRatio = (svgHeight / defaultCharHeight),
-                svgWidth = (contentWidth * scalingRatio),
-                svgContent = '<svg xmlns="http://www.w3.org/2000/svg" ' +
-                    'width="' + svgWidth + '" height="' + svgHeight + '" ' +
-                    'viewBox="0 0 ' + contentWidth + ' ' + defaultCharHeight + '">' +
-                    '<g transform="scale(1,-1) translate(0 -1506)">' +
-                    '<path d="' + pathData + '" fill="' + config.color + '" />' +
-                    '</g><!--width-to-height ratio is ' + (svgWidth / svgHeight) + '--></svg>';
 
-            if (typeof iconSvgCodes[iconName] == 'undefined') {
-                iconSvgCodes[iconName] = {}
-            }
-            iconSvgCodes[iconName][svgHeight] = svgContent;
+        iconSvg.push({
+            iconName: iconName,
+            svg: '<svg xmlns="http://www.w3.org/2000/svg" ' +
+                 'viewBox="0 0 ' + contentWidth + ' ' + defaultCharHeight + '">' +
+                 '<g transform="scale(1,-1) translate(0 -1519)">' +
+                 '<path d="' + pathData + '" fill="' + config.color + '" />' +
+            '</g></svg>'
         });
     });
 
-    return iconSvgCodes;
+    var optimizedCount = 0;
+    iconSvg.forEach(function (ic, idx) {
+        svgo.optimize(ic.svg, function (result) {
+            iconSvg[idx].svg = result.data;
+            if (++optimizedCount == iconSvg.length) {
+                callback(iconSvg);
+            }
+        });
+    });
 }
 
-function generateSvgFiles(folder, content) {
-    var numSvgFiles = 0;
-    Object.keys(content).forEach(function (iconName) {
-        var iconSvgs = content[iconName];
-        Object.keys(iconSvgs).forEach(function (svgHeight) {
-            var svgDir = path.join(folder, 'svg', svgHeight);
-            ensureDir(svgDir);
-            fs.writeFileSync(path.join(svgDir, iconName + '.svg'), iconSvgs[svgHeight]);
-            numSvgFiles++;
-        })
+function saveSvgFiles(folder, iconSvgs) {
+    var numSvgFiles = 0,
+        svgDir = path.join(folder, 'svg');
+
+    ensureDir(svgDir);
+
+    iconSvgs.forEach(function (icInfo) {
+        numSvgFiles++;
+        fs.writeFileSync(path.join(svgDir, icInfo.iconName + '.svg'), icInfo.svg);
     });
 
     console.log("Saved " + numSvgFiles + " SVG files");
 
-    generatePngsFromFolder(folder);
-    //svgo = new (require('svgo')),
-    //svgo.optimize(svgContent, function (result) {
-    //    fs.writeFileSync(saveFilePath, result.data);
-    //});
+    //generatePngsFromFolder(folder);
+
 }
 
 function generatePngsFromFolder(folder) {
     var spawn = require('child_process').spawn;
 
     config.height.forEach(function (size) {
-        size = size.trim();
+        size = String(size).trim();
         var targetDir = path.join(folder, 'png', size.toString());
         ensureDir(targetDir);
         //var ps = spawn('java', ['-jar', __dirname + '/batik-rasterizer.jar', "$@", '-d', targetDir, '-h', size, path.join(config.color, 'svg')]);
         //var ps = spawn('batik-rasterizer', ['-d', targetDir, '-h', size, path.join(folder, 'svg')]);
-        var ps = spawn('batik-rasterizer', ['-d', targetDir, path.join(folder, 'svg', size)]);
+        var ps = spawn('batik-rasterizer', ['-d', targetDir, '-h', size, path.join(folder, 'svg')]);
         ps.stdout.on('data', function (data) {
             console.log(data.toString());
         });
@@ -124,8 +123,9 @@ module.exports = function (destinationFolder, userConfig) {
                 }
                 loadFontAwesomeData[sourceIdx] = res.text;
                 if (loadFontAwesomeData[0].length > 1000 && loadFontAwesomeData[1].length > 1000) {
-                    var svgContent = splitupSvgContent(loadFontAwesomeData[0], loadFontAwesomeData[1]);
-                    generateSvgFiles(destinationFolder, svgContent);
+                    splitupSvgContent(loadFontAwesomeData[0], loadFontAwesomeData[1], function (svgContent) {
+                        saveSvgFiles(destinationFolder, svgContent);
+                    });
                 }
             });
     });
