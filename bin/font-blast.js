800500b32d763165ce1a98df619a56d0501aff18
Eugene G <eugene7@gmail.com>
2014-10-09 23:45:16 +1100

    added a local batik instance

--- bin/font-blast.js
+++ bin/font-blast.js
@@ -11,8 +11,8 @@ program.
     option('-i, --icons <icon-refs>', "Limit the output to the selected icons. Icons can be provided with their unicode value or the full reference", function (val) {
         return val ? val.split(',') : [];
     }).
-    option('-p, --png', "Include this to generate PNG files. Please note you will need to have an executable binary in your path for 'batik-rasterizer'").
-    option('-c, --color', "Set the color of icons in the output (relevant mainly for PNG files)").
+    option('-p, --png <heightInPx>', "Include this to generate PNG files. Please note you will need to have an executable binary in your path for 'batik-rasterizer'", parseInt).
+    option('-c, --color <colorcode>', "Set the color of icons in the output (relevant mainly for PNG files)").
     parse(process.argv)
 ;
 
@@ -21,10 +21,15 @@ if (!svgFontFile || !outputDir) {
     program.help();
 }
 
+if (!fs.existsSync(svgFontFile)) {
+    console.error("This provided SVG font file does not exist");
+    return;
+}
+
+
 var config    = {
     icons: program.icons,
     png:   program.png,
     color: program.color
 }, svgContent = fs.readFileSync(svgFontFile, 'utf8');
-
-require('../lib/index')(svgContent, outputDir, config);
+require('../lib/font-blast/index')(svgContent, outputDir, config);
